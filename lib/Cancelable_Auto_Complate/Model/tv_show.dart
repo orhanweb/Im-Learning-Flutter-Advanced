0b2aa401e846cadfac2df1b56a73df6eec77fd59
Orhan-Kahraman <herorhan999@gmail.com>
2023-03-08 18:18:25 +0300

    added last changes

--- lib/Cancelable_Auto_Complate/Model/tv_show.dart
+++ lib/Cancelable_Auto_Complate/Model/tv_show.dart
@@ -14,10 +14,10 @@ class TvShow extends INetworkModel<TvShow> {
 
   @override
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['score'] = this.score;
-    if (this.show != null) {
-      data['show'] = this.show!.toJson();
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['score'] = score;
+    if (show != null) {
+      data['show'] = show!.toJson();
     }
     return data;
   }
@@ -47,20 +47,20 @@ class Show extends INetworkModel<Show> {
     status = json['status'];
   }
 
+  @override
   Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['url'] = this.url;
-    data['name'] = this.name;
-    data['type'] = this.type;
-    data['language'] = this.language;
-    data['status'] = this.status;
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['url'] = url;
+    data['name'] = name;
+    data['type'] = type;
+    data['language'] = language;
+    data['status'] = status;
     return data;
   }
 
   @override
   Show fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
     throw UnimplementedError();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:im_learning_flutter_advanced/Modular_Field_Learn/modular_view.dart';
 import 'package:im_learning_flutter_advanced/Network_Change_Learn/Core/main_build.dart';
 import 'package:im_learning_flutter_advanced/TextField_withoutController_intent/login_view.dart';
 
